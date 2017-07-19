b7aa454753f02bae55f63268088fa3f90d6c1c34
Anthony Gore <anthony@magnum.technology>
2017-07-19 15:31:55 +0200

    Getting this to work

--- index.js
+++ index.js
@@ -1,45 +1,37 @@
-"use strict";
-
-import { resolve } from 'path';
+const resolve = require('path').resolve;
 const critical = require('critical');
-import { cleantmp } from 'webpack-util-cleantmp';
+const cleantmp = require('webpack-util-cleantmp').cleantmp;
 
-export default {
-  constructor(public options: CriticalPluginOptions) {
-    if (!options.dest) {
-      throw new Error('A `dest` option is required for the Critical webpack plugin');
-    }
-  }
+function HtmlWebpackCriticalPlugin (options) {};
 
-  emit(compilation, callback) {
-    const options = this.options;
-    const subscription = cleantmp({
-      prefix: 'criticalcss',
-      globToDisk: '**/*.+(css|html)',
-      globFromDisk: '**/*.+(css|html)',
-      assets: compilation.assets
-    })
-    .subscribe((tmp) => {
-      const opts = {...options, ...{
-        base: resolve(tmp, options.base || ''),
-        dest: resolve(tmp, options.dest)
-      }};
+HtmlWebpackHarddiskPlugin.prototype.emit = function(compilation, callback) {
+  const options = this.options;
+  const subscription = cleantmp({
+    prefix: 'criticalcss',
+    globToDisk: '**/*.+(css|html)',
+    globFromDisk: '**/*.+(css|html)',
+    assets: compilation.assets
+  })
+  .subscribe((tmp) => {
+    const opts = {...options, ...{
+      base: resolve(tmp, options.base || ''),
+      dest: resolve(tmp, options.dest)
+    }};
 
-      critical.generate(opts, (err, output) => {
-        subscription.unsubscribe();
-        callback(err);
-      });
-    })
-  }
-
-  apply(compiler) {
-    compiler.plugin('compilation', function(compilation) {
-      console.log('The compiler is starting a new compilation...');
-
-      compilation.plugin('html-webpack-plugin-after-emit', function(htmlPluginData, callback) {
-        this.emit.bind(this)
-        callback(null, htmlPluginData);
-      });
+    critical.generate(opts, (err, output) => {
+      subscription.unsubscribe();
+      callback(err);
     });
-  }
-}
+  })
+};
+
+HtmlWebpackHarddiskPlugin.prototype.apply(compiler) {
+  compiler.plugin('compilation', function(compilation) {
+    compilation.plugin('html-webpack-plugin-after-emit', function(htmlPluginData, callback) {
+      this.emit.bind(this)
+      callback(null, htmlPluginData);
+    });
+  });
+};
+
+module.exports = HtmlWebpackCriticalPlugin
