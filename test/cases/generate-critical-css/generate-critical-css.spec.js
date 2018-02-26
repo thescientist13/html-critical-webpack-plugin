deabc94fee455b501f5923e1865f22fb9ef64d45
Owen Buckley <owenbuckley13@gmail.com>
2018-02-25 19:19:08 -0500

    testing passing

--- test/cases/generate-critical-css/generate-critical-css.spec.js
+++ test/cases/generate-critical-css/generate-critical-css.spec.js
@@ -10,33 +10,47 @@ const webpack = require('webpack');
 describe('HtmlCriticalWebpackPlugin Cases: Generate Critical CSS', () => {
   const buildDirectory = path.resolve(__dirname, 'build');
 
-  beforeEach(() => {
-    rimraf(buildDirectory, () => {
-      console.log('build directory cleaned');
-    });
-  });
+  describe('minimum configuration', () => {
 
-  // <link href="styles.da22eda767cd98613b04.0e398130.css" rel="preload" as="style" onload="this.onload=null;this.rel='stylesheet'">
-  // <noscript><link href="styles.da22eda767cd98613b04.0e398130.css" rel="stylesheet"></noscript>
-  // <script>!function(t){"use strict";t.loadCSS||(t.loadCSS=function(){});var e=loadCSS.relpreload={};if(e.support=function(){var e;try{e=t.document.createElement("link").relList.supports("preload")}catch(t){e=!1}return function(){return e}}(),e.bindMediaToggle=function(t){var e=t.media||"all";function a(){t.media=e}t.addEventListener?t.addEventListener("load",a):t.attachEvent&&t.attachEvent("onload",a),setTimeout(function(){t.rel="stylesheet",t.media="only x"}),setTimeout(a,3e3)},e.poly=function(){if(!e.support())for(var a=t.document.getElementsByTagName("link"),n=0;n<a.length;n++){var o=a[n];"preload"!==o.rel||"style"!==o.getAttribute("as")||o.getAttribute("data-loadcss")||(o.setAttribute("data-loadcss",!0),e.bindMediaToggle(o))}},!e.support()){e.poly();var a=t.setInterval(e.poly,500);t.addEventListener?t.addEventListener("load",function(){e.poly(),t.clearInterval(a)}):t.attachEvent&&t.attachEvent("onload",function(){e.poly(),t.clearInterval(a)})}"undefined"!=typeof exports?exports.loadCSS=loadCSS:t.loadCSS=loadCSS}("undefined"!=typeof global?global:this);</script></head>
-  // <body>
-  // <script type="text/javascript" src="main.js"></script><script type="text/javascript" src="index.js"></script></body>
-
-  describe('minomum configuration', () => {
-    
-    it('should generate the expected critical inline css style tag', (done) => {
+    it('should generate the expected critical inline <style> tag', (done) => {
       webpack(webpackConfig, () => {
         const indexHtmlString = fs.readFileSync(`${buildDirectory}/index.html`, {encoding: 'utf8'});
         const indexHtmlDom = new JSDOM(indexHtmlString);
-        const inlineStyleTag = indexHtmlDom.window.document.querySelectorAll('style');
+        const inlineStyleTags = indexHtmlDom.window.document.querySelectorAll('style');
         
-        assert.equal(inlineStyleTag.length, 1);
-        assert.equal(inlineStyleTag[0].getAttribute('type'), 'text/css');
+        assert.equal(inlineStyleTags.length, 1);
+        assert.equal(inlineStyleTags[0].getAttribute('type'), 'text/css');
 
         done();
       });
     });
 
-  })
+    it('should generate the expected critical <link> tag', (done) => {
+      webpack(webpackConfig, () => {
+        const indexHtmlString = fs.readFileSync(`${buildDirectory}/index.html`, {encoding: 'utf8'});
+        const indexHtmlDom = new JSDOM(indexHtmlString);
+        const linkTags = indexHtmlDom.window.document.querySelectorAll('link');
+        
+        assert.equal(linkTags.length, 1);
+        assert.equal(linkTags[0].getAttribute('rel'), 'preload');
+        assert.equal(linkTags[0].getAttribute('as'), 'style');
+
+        done();
+      })
+    });
+
+    it('should generate the expected critical <noscript> tag', (done) => {
+      webpack(webpackConfig, () => {
+        const indexHtmlString = fs.readFileSync(`${buildDirectory}/index.html`, {encoding: 'utf8'});
+        const indexHtmlDom = new JSDOM(indexHtmlString);
+        const noscriptTags = indexHtmlDom.window.document.querySelectorAll('noscript');
+        
+        assert.equal(noscriptTags.length, 1);
+
+        done();
+      });
+    });
+
+  });
 
 });
