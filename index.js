464350c1c9864690e261ec8d34653bfb9c9f7f84
Owen Buckley <owen@thegreenhouse.io>
2018-05-14 10:40:37 -0400

    class syntax

--- index.js
+++ index.js
@@ -1,21 +1,41 @@
 const critical = require('critical');
 
-function HtmlCriticalWebpackPlugin (options) {
-  this.options = options;
+class HtmlCriticalWebpackPlugin {
+  constructor(options) {
+    this.options = options;
+  }
+
+  emit(compilation, callback) {
+    critical.generate(this.options, (err) => {
+      callback(err);
+    });
+  }
+
+  apply(compiler) {
+
+    compiler.hooks.afterEmit.tapAsync('HtmlCriticalWebpackPlugin', (compilation, callback) => {
+      this.emit(compilation, callback);
+    });
+
+  }
 }
 
-HtmlCriticalWebpackPlugin.prototype.emit = function(compilation, callback) {
-  critical.generate(this.options, (err) => {
-    callback(err);
-  });
-};
-
-HtmlCriticalWebpackPlugin.prototype.apply = function(compiler) {
-  var self = this;
-
-  compiler.hooks.afterEmit.tapAsync('HtmlCriticalWebpackPlugin', function (compilation, callback) {
-    self.emit(compilation, callback);
-  });
-};
-
 module.exports = HtmlCriticalWebpackPlugin;
+
+// function HtmlCriticalWebpackPlugin (options) {
+//   this.options = options;
+// }
+
+// HtmlCriticalWebpackPlugin.prototype.emit = function(compilation, callback) {
+//   critical.generate(this.options, (err) => {
+//     callback(err);
+//   });
+// };
+
+// HtmlCriticalWebpackPlugin.prototype.apply = function(compiler) {
+//   var self = this;
+
+//   compiler.hooks.afterEmit.tapAsync('HtmlCriticalWebpackPlugin', function (compilation, callback) {
+//     self.emit(compilation, callback);
+//   });
+// };
